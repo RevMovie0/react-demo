99bb928c8ed0fde2188fa3b72c0627133023b711
mike <erdene.qa@gmail.com>
2025-10-23 11:09:38 -0400

    Remove duplicate files from root, keep only src folder

--- Celsius.jsx
+++
@@ -1,9 +0,0 @@
-let Celsius = (props) => {
-  return (
-    <div>
-      <h2>Celsius: {props.temperature} °C</h2>
-    </div>
-  );
-};
-
-export default Celsius;
--- Fahrenheit.jsx
+++
@@ -1,9 +0,0 @@
-let Fahrenheit = (props) => {
-  return (
-    <div>
-      <h2>Fahrenheit: {props.temperature} °F</h2>
-    </div>
-  );
-};
-
-export default Fahrenheit;
