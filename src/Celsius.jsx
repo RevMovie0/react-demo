d8db8b11b97f4d6d9d9092e18246322f0d38c1ad
mike <erdene.qa@gmail.com>
2025-10-23 09:32:37 -0400

    Add React temperature converter app with fixed component display logic

---
+++ src/Celsius.jsx
@@ -0,0 +1,9 @@
+let Celsius = (props) => {
+  return (
+    <div>
+      <h2>Celsius: {props.temperature} °C</h2>
+    </div>
+  );
+};
+
+export default Celsius;
---
+++ src/Fahrenheit.jsx
@@ -0,0 +1,9 @@
+let Fahrenheit = (props) => {
+  return (
+    <div>
+      <h2>Fahrenheit: {props.temperature} °F</h2>
+    </div>
+  );
+};
+
+export default Fahrenheit;
